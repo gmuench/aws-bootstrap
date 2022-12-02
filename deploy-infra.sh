d136e02573583f504cb7848f145f93cf1c8d3579
Grant Muench <gmuench@gmail.com>
2022-12-02 12:51:10 -0600

    Add code build, code deploy, and code pipeline resources

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,3 +1,7 @@
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
 STACK_NAME=awsbootstrap
 REGION=us-east-1
 CLI_PROFILE=awsbootstrap
@@ -17,22 +21,27 @@ aws cloudformation deploy \
   --parameter-overrides \
   CodePipelineBucket="$CODEPIPELINE_BUCKET"
 
-# Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ==========="
-aws cloudformation deploy \
-  --region $REGION \
-  --profile $CLI_PROFILE \
-  --stack-name $STACK_NAME \
-  --template-file main.yml \
-  --no-fail-on-empty-changeset \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-  EC2InstanceType=$EC2_INSTANCE_TYPE
+  # Deploy the CloudFormation template
+  echo -e "\n\n=========== Deploying main.yml ==========="
+  aws cloudformation deploy \
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --stack-name $STACK_NAME \
+    --template-file main.yml \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
-if [ $? -eq 0 ]; then
-  aws cloudformation list-exports \
-  --region $REGION
-  --profile $CLI_PROFILE \
-  --query "Exports[?Name=='InstanceEndpoint'].Value"
-fi
+#if [ $? -eq 0 ]; then
+#  aws cloudformation list-exports \
+#  --region $REGION
+#  --profile $CLI_PROFILE \
+#  --query "Exports[?Name=='InstanceEndpoint'].Value"
+#fi
